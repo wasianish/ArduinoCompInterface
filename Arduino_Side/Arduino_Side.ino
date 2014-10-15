3c62aaddb51f513b47178721948515ff09c988cd
Anno van den Akker <annovda@gmail.com>
2014-10-15 14:50:32 -0400

    Many features that im too lazy to write

--- Arduino_Side/Arduino_Side.ino
+++ Arduino_Side/Arduino_Side.ino
@@ -4,17 +4,28 @@ int acStartByte = 64; // Packet start byte from arduino to comp
 char inBuffer[3]; // Buffer of packets from comp to arduino
 char outBuffer[4]; // Buffer of packets from arduino to comp
 
-int inBufferPull = 0; // Where to get buffered data from
-int inBufferPush = 0; // Where to put data to be buffered
-
-int outBufferPull = 0; // Where to get buffered data from
-int outBufferPush = 0; // Where to put data to be buffered
-
 int caCounted = 0;
 
 int baudRate = 9600;
 
 int compByte = 0;
+
+byte dRead[32];
+byte aRead[32];
+
+byte drBufferPull = 0;
+byte drBufferPush = 0;
+
+byte arBufferPull = 0;
+byte arBufferPush = 0;
+
+boolean listenDigital[54];
+boolean listenAnalog[16];
+
+byte listenDPriority[54];
+byte listenAPriority[16];
+
+byte serialForward = -1;
 
 void setup() {
   Serial.begin(baudRate);
@@ -23,6 +34,24 @@ void setup() {
 void loop() {
   if(Serial.available()) {
     compByte = Serial.read();
+    if(serialForward != -1) {
+      // Need way to exit serial forwarding
+      switch(serialForward) {
+        case 0:
+          Serial.write(compByte);
+          break;
+        case 1:
+          Serial1.write(compByte);
+          break;
+        case 2:
+          Serial2.write(compByte);
+          break;
+        case 3:
+          Serial3.write(compByte);
+          break;
+      }
+      continue;
+    }
     if(compByte == caStartByte) {
       Serial.readBytes(inBuffer, 3);
       execute(inBuffer);
@@ -35,23 +64,55 @@ void execute(char packet[3]) {
   boolean digital = (((byte)packet[0]) & 10000000) >> 7;
   
   switch((int) packet[1]) {
-    case 0:
+    case 0: // Add to read buffer
+      if(digital) {
+        dRead[drBufferPush] = pin;
+        drBufferPush++;
+      } else {
+        aRead[arBufferPush] = pin;
+        arBufferPush++;
+      }
+      break;
+    case 1: // Write
+      if(digital) {
+        digitalWrite(pin, ((byte)(packet[2]) & 00000001));
+      } else {
+        analogWrite(pin, packet[2]);
+      }
+      break;
+    case 2: // Serial it
     
-    case 1:
-      
-    case 2:
-    
-    case 3:
-    
-    case 4:
-    
-    case 5:
-    
-    case 6:
-    
-    case 7:
-    
-    case 8:
+    case 3: // Define as input
+      if(digital) {
+        pinMode(pin, INPUT);
+      }
+      break;
+    case 4: // Define as output
+      if(digital) {
+        pinMode(pin, OUTPUT);
+      }
+      break'
+    case 5: // Add to the listener array
+      if(digital) {
+        if(packet[2] == 0) {
+          listenDigital[pin] = false;
+        } else {
+          listenDigital[pin] = true;
+        }
+      } else {
+        if(packet[2] == 0) {
+          listenAnalog[pin] = false;
+        } else {
+          listenAnalog[pin] = true;
+        }
+      }
+      break;
+    case 6: // Set Priority
+      if(digital) {
+        listenDPriority[pin] = packet[2];
+      } else {
+        listenAPriority[pin] = packet[2];
+      }
       break;
   }
 }
